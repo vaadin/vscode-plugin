3023c55f6381342c4c635d25cc6702ce72b55e8f
marcin <marcin@vaadin.com>
2024-06-04 16:05:54 +0200

    Show Vaadin status bar item only for Vaadin projects

--- src/extension.ts
+++ src/extension.ts
@@ -22,6 +22,7 @@ export async function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(newProjectCommand);
 
 	if (isVaadinProject()) {
+		statusBarItem.show();
 		startServer();
 	}
 
