f1983078d2838712a11622400bbeb9dfdd1772fa
marcin <marcin@vaadin.com>
2025-03-11 11:01:25 +0100

    feat: Debug hotswap (#41)
    * feat: Debug (hotswap) code lens link
    
    * auto setup jbr
    
    * Do not throw exception when Redhat Java is not present
    
    ---------
    
    Co-authored-by: Artur Signell <artur@vaadin.com>

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,10 @@ import { newProjectUserInput } from './helpers/userInput';
 import { undoManager } from './helpers/undoManager';
 import { deleteProperties } from './helpers/properties';
 import { debugUsingHotswap, setupHotswap } from './helpers/hotswap';
+import { DebugCodeLensProvider } from './debug-using-hotswapagent';
+import { JAVA_DEBUG_CONFIGURATION, JAVA_LANGID } from './helpers/javaUtil';
+
+const ENABLE_CODE_LENS_VARIABLE = 'enableRunDebugCodeLens';
 
 export async function activate(context: vscode.ExtensionContext) {
   let startServerCommand = vscode.commands.registerCommand('vaadin.start', function () {
@@ -13,12 +17,18 @@ export async function activate(context: vscode.ExtensionContext) {
   let newProjectCommand = vscode.commands.registerCommand('vaadin.newProject', function () {
     createNewProject();
   });
-  let setupHotswapCommand = vscode.commands.registerCommand('vaadin.setupHotswap', function () {
-    setupHotswap(context);
-  });
-  let debugUsingHotswapCommand = vscode.commands.registerCommand('vaadin.debugUsingHotswap', function () {
-    debugUsingHotswap(context);
-  });
+  let setupHotswapCommand = vscode.commands.registerCommand(
+    'vaadin.setupHotswap',
+    function (noPrompt: boolean | undefined) {
+      setupHotswap(context, noPrompt);
+    },
+  );
+  let debugUsingHotswapCommand = vscode.commands.registerCommand(
+    'vaadin.debugUsingHotswap',
+    function (autoSetup: boolean | undefined) {
+      debugUsingHotswap(context, autoSetup);
+    },
+  );
 
   // disposables
   context.subscriptions.push(statusBarItem);
@@ -26,6 +36,14 @@ export async function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(newProjectCommand);
   context.subscriptions.push(setupHotswapCommand);
   context.subscriptions.push(debugUsingHotswapCommand);
+
+  const configuration = vscode.workspace.getConfiguration(JAVA_DEBUG_CONFIGURATION);
+  const isCodeLensEnabled = configuration.get<boolean>(ENABLE_CODE_LENS_VARIABLE);
+
+  if (isCodeLensEnabled) {
+    const lensProvider = vscode.languages.registerCodeLensProvider(JAVA_LANGID, new DebugCodeLensProvider());
+    context.subscriptions.push(lensProvider);
+  }
 
   if (isVaadinProject()) {
     startServer();
